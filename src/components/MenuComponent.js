b04bfa51d8c632b66f8bd7139391a6637892e681
Rasputin <jmcdon35@uic.edu>
2022-05-15 13:02:43 -0500

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,37 +7,16 @@ class Menu extends Component {
   constructor(props) {
       super(props);
 
-      this.state = {
-          selectedDish: null
-      }
-  }
+      console.log("Menu Component Constructed.");
 
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-  }
-
-  renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
   }
 
   render() {
+      console.log("Renders menue component.");
       const menu = this.props.dishes.map((dish) => {
           return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-              <Card onClick={() => this.onDishSelect(dish)}>
+            <div className="col-12 col-md-5 m-1">
+              <Card key={dish.id} onClick={()=> this.props.onClick(dish.Id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -51,9 +30,6 @@ class Menu extends Component {
           <div className="container">
               <div className="row">
                   {menu}
-              </div>
-              <div className="row">
-                  {this.renderDish(this.state.selectedDish)}
               </div>
           </div>
       );
